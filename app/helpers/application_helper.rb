45be3e86374f6c46f1487d75a46ef8399dffc411
Timjini <hatim.jini@gmail.com>
2023-11-04 13:34:54 +0300

    add parent dashboard

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,10 @@
 module ApplicationHelper
 
-    def render_coach_dashboard_if_coach(user)
+   def render_coach_dashboard_if_coach(user)
         if user&.coach?
             render 'dashboard/coach_dashboard'
+        elsif user.role == 'athlete_parent'
+            render 'dashboard/parent_dashboard'
         else
             render 'dashboard/athlete_dashboard'
         end
