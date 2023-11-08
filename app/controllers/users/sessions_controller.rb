aa72a0b1cee62a2a6ddfee04d64421e679a98995
Timjini <hatim.jini@gmail.com>
2023-11-08 10:52:21 +0300

    final UI updated

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -15,4 +15,8 @@ class Users::SessionsController < Devise::SessionsController
           redirect_to new_user_session_path
         end
     end
+
+    def show
+        @user = User.find(params[:id])
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,12 @@ class UsersController < ApplicationController
     def index
     end
 
-    def show 
-        user = User.find(params[:id])
-        render json: user
+    def show
+        @user = User.find(params[:id])
+        respond_to do |format|
+            format.html { render :show }
+            format.json { render json: @user }
+        end
     end
+
 end
