1bcbe73da525e9f26340dc967009b11528b38299
Timjini <hatim.jini@gmail.com>
2023-12-07 23:53:06 +0300

    mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ApplicationMailer
   
   def test_email
-    mail(to: 'hatim.jini@gmail.com', subject: 'Test Email') do |format|
+    mail(to: 'hatim.jini@gmail.com', subject: 'Test Email', from:'dwain.chambers@chambersforsport.com') do |format|
       format.text { render plain: 'This is a test email sent from MailCatcher.' }
     end
   end
