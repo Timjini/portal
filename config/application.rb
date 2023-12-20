81dcc6f905029f98212876d3e21e77cd239ce4ea
Smartup Network <smartupnetwork@Smartup-MacBook-Pro.local>
2023-12-20 10:26:02 +0300

    mailer setup

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module Portal
 
     #config.action_controller.default_url_options = { host: 'chambersforsport.net', protocol: 'https' }
     # config.action_mailer.default_url_options = { host: 'club.chambersforsport.com', protocol: 'https' }
-    config.action_mailer.default_url_options = { host: 'localhost:3000'}
+    config.action_mailer.default_url_options = { host: 'chambersforsport.net' }
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
 
