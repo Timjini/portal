352a9e7ae9b10d4f5e7567391870a18bb8a9ee62
Timjini <hatim.jini@gmail.com>
2024-01-10 22:48:16 +0300

    half finished notification

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,7 +1,7 @@
 module NotificationHelper
 
     def general_notification(user, category, message)
-        parent = user.parent if user.role.in?(['parent_user', 'child_user'])
+        parent = User.find_by(parent_id:user.parent_id, role:"parent") if user.role.in?(['parent_user', 'child_user'])
 
         notification_params = {
             body: message,
@@ -13,6 +13,13 @@ module NotificationHelper
 
         if parent
             Notification.create(notifiable: parent, **notification_params)
+        end
+
+        # find and destroy the oldest notification if the user has more than 5 notifications
+        notifications = Notification.where(notifiable: user)
+        if notifications.count > 5
+            # keep only last notification and destroy all 
+            notifications.last.destroy
         end
     end
 
