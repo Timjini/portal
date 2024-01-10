38c2f6180847d6356c26f331abc4caa7fa41ccfb
Timjini <hatim.jini@gmail.com>
2024-01-10 22:57:21 +0300

    mailer configuration

--- app/helpers/notification_helper.rb
+++ app/helpers/notification_helper.rb
@@ -1,26 +1,35 @@
 module NotificationHelper
 
     def general_notification(user, category, message)
-        parent = User.find_by(parent_id:user.parent_id, role:"parent") if user.role.in?(['parent_user', 'child_user'])
+  parent = User.find_by(parent_id: user.parent_id, role: "parent") if user.role.in?(['parent_user', 'child_user'])
 
-        notification_params = {
-            body: message,
-            viewed: false,
-            category: category
-        }
+  notification_params = {
+    body: message,
+    viewed: false,
+    category: category
+  }
 
-        Notification.create(notifiable: user, **notification_params)
+  # Create the notification
+  Notification.create(notifiable: user, **notification_params)
 
-        if parent
-            Notification.create(notifiable: parent, **notification_params)
-        end
+  # Send email to the user
+  NotificationMailer.notification_email(user.email, category, message).deliver_now
 
-        # find and destroy the oldest notification if the user has more than 5 notifications
-        notifications = Notification.where(notifiable: user)
-        if notifications.count > 5
-            # keep only last notification and destroy all 
-            notifications.last.destroy
-        end
-    end
+  if parent
+    # Create the notification for the parent
+    Notification.create(notifiable: parent, **notification_params)
+
+    # Send email to the parent
+    NotificationMailer.notification_email(parent.email, category, message).deliver_now
+  end
+
+  # Find and destroy the oldest notification if the user has more than 5 notifications
+  notifications = Notification.where(notifiable: user)
+  if notifications.count > 5
+    # Keep only the last notification and destroy all others
+    notifications.last.destroy
+  end
+end
+
 
 end
---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,9 @@
+# app/mailers/notification_mailer.rb
+class NotificationMailer < ApplicationMailer
+  def notification_email(user_email, category, message)
+    @category = category
+    @message = message
+
+    mail(to: user_email, subject: 'Notification')
+  end
+end
