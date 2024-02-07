f32aecf0aab5d6385bc977daff9b44006d39d8e7
Smartup Network <smartupnetwork@192.168.1.13>
2024-02-07 13:44:48 +0300

    update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,8 @@
 # cors file 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000'
+        # origins 'http://localhost:3000'
+        origins 'https://chambersforsport.com'
         resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
     end
 end
