256a09602c37db5c0865f734271180b17158556e
Smartup Network <smartupnetwork@192.168.1.13>
2024-02-07 15:03:44 +0300

    send the code

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,13 @@
 class Answer < ApplicationRecord
   belongs_to :user
   belongs_to :question
+
+
+  def has_health_issue
+    answers = Answer.where(user_id: self.user_id)
+    answers.any? { |answer| answer.content == "Yes" } ? "Yes" : "No"
+  end
+
+
+
 end
