9073c9ef32c550fa3807ae51e7c7206769cc0593
Smartup Network <smartupnetwork@Hatim-Laajini.fritz.box>
2024-07-11 12:17:21 +0200

    update credentials

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -3,6 +3,5 @@ class NotificationsController < ApplicationController
     def set_viewed
         @notification = Notification.find(params[:id])
         @notification.update(viewed: true)
-        return json: { success: true }
-    end
+        render json: { success: true }    end
 end
