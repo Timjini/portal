734e7dd330bb163a0743135f51c8482f97b96d2e
Timjini <hatim.jini@gmail.com>
2024-07-15 01:50:19 +0200

    buybug

--- app/helpers/time_slots_helper.rb
+++ app/helpers/time_slots_helper.rb
@@ -1,2 +1,54 @@
+require "rubygems"
+require "recurrence"
+
 module TimeSlotsHelper
+    def create_recurrent_timeslots(time_slot, recurrence_rule, recurrence_end)
+    recurrence_end = DateTime.parse(recurrence_end) if recurrence_end.present?
+    recurrence_rule = { every: recurrence_rule.to_sym,  until: recurrence_end }.compact
+
+    byebug
+
+    recurrence = Recurrence.new(recurrence_rule.merge(starts: time_slot.start_time))
+
+    recurrence.events.each do |event_date|
+      Timeslot.create(
+        start_time: event_date,
+        end_time: event_date + (time_slot.end_time - time_slot.start_time),
+        coach_calendar_id: time_slot.coach_calendar_id,
+        recurrence_rule: time_slot.recurrence_rule,
+        recurrence_end: time_slot.recurrence_end
+      )
+    end
+
+    redirect_to timeslots_path, notice: 'Recurrent timeslots were successfully created.'
+  end
+
+   def save_time_slot(time_slot)
+    respond_to do |format|
+      if time_slot.save
+        format.html { redirect_to time_slot_url(time_slot), notice: "Time slot was successfully created." }
+        format.json { render :show, status: :created, location: time_slot }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: time_slot.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update_recurrent_timeslots(original_time_slot, time_slot_params)
+    timeslots = Timeslot.where(
+      "coach_calendar_id = ? AND start_time >= ? AND recurrence_rule = ?",
+      original_time_slot.coach_calendar_id,
+      original_time_slot.start_time,
+      original_time_slot.recurrence_rule
+    )
+
+    # Update each timeslot with the new parameters
+    timeslots.each do |timeslot|
+      timeslot.update(time_slot_params)
+    end
+
+    redirect_to timeslots_path, notice: 'Recurring timeslots were successfully updated.'
+  end
+
 end
--- app/models/time_slot.rb
+++ app/models/time_slot.rb
@@ -2,4 +2,7 @@ class TimeSlot < ApplicationRecord
   belongs_to :coach_calendar
 
   enum group_type: { development: "development", intermediate: "intermediate", advanced: "advanced"}
+  validates :recurrence_rule, inclusion: { in: %w[daily weekly monthly], message: "%{value} is not a valid recurrence rule" }, allow_nil: true
+  validates :recurrence_end, presence: true, if: -> { recurrence_rule.present? }
+
 end
