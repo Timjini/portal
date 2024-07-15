4d408d022bb334af27c5d08756b03b3d0b10f361
Timjini <hatim.jini@gmail.com>
2024-07-15 02:04:23 +0200

    all calendars details

--- app/controllers/coach_calendar_controller.rb
+++ app/controllers/coach_calendar_controller.rb
@@ -30,4 +30,24 @@ class CoachCalendarController < ApplicationController
                 render json: { error: "No calendar found for user with ID #{user_id}" }, status: :not_found
         end
     end
+
+    def all_calendars
+        @calendars = CoachCalendar.all
+        if @calendars
+            time_slots = TimeSlot.all
+
+            time_slots_map = time_slots.map do |time_slot|
+                {
+                id: time_slot.id,
+                title: time_slot.group_type,
+                start: "#{time_slot.date}T#{time_slot.start_time.strftime("%H:%M:%S")}",
+                end: "#{time_slot.date}T#{time_slot.end_time.strftime("%H:%M:%S")}",
+                color: 'green'
+                }
+            end
+                render json: time_slots_map
+            else
+                render json: { error: "No calendar found" }, status: :not_found
+        end
+    end
 end
