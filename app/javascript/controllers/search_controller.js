ad9cbeb9348496f68f486746b71499dd59bddce5
Timjini <hatim.jini@gmail.com>
2024-09-10 00:16:12 +0200

    precompile

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -1,5 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
-import { debounce } from "lodash"
+// import { debounce } from "lodash"
 
 export default class extends Controller {
   static targets = ["input", "list"]
