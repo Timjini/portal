3b7440396c26597333d3eae0b759749d4cec7a77
Timjini <hatim.jini@gmail.com>
2024-10-22 21:37:24 +0200

    mailer setup

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  
+  include MailSender
   def test_email
     mail(to: 'hatim.jini@gmail.com', subject: 'Test Email', from:'info@chambersforsport.com') do |format|
       format.text { render plain: 'This is a test email sent from MailCatcher.' }
@@ -9,10 +9,9 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url  = 'https://club.chambersforsport.net/users/sign_in'
-    mail(to: @user.email, subject: 'Welcome to Chambers For Sport', from:'
-    <EMAIL>') do |format|
-      format.text { render plain: "Hello #{@user.first_name} #{@user.last_name}!" }
-    end
+    
+    # This will automatically render the welcome_email.html.erb and welcome_email.text.erb templates
+    mail(to: @user.email, subject: 'Welcome to Chambers For Sport')
   end
   
 end
