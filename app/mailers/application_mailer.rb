b61b5e0f54c367a32d0269a513b7e782869979c1
Timjini <hatim.jini@gmail.com>
2024-11-01 15:30:01 +0100

    change sender email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationMailer < ActionMailer::Base
-  default from: 'info@devminds.cloud'
+  default from: 'info@chambersforsport.com'
   layout 'mailer'
 end
