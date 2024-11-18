0098be6bf2edfb417757372b135c893def80be49
Timjini <hatim.jini@gmail.com>
2024-11-18 14:26:01 +0100

    add extras and new type to TrainingPackage

--- app/models/training_package.rb
+++ app/models/training_package.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class TrainingPackage < ApplicationRecord
+    STATUS = %w[draft active inactive].freeze
+    PACKAGE_TYPES = %w[group_training gift_voucher].freeze
+    TRAINING_TYPES = %w[group_training individual_training].freeze
+
 end
---
+++ db/migrate/20241118132039_add_extra_data_to_training_package.rb
@@ -0,0 +1,6 @@
+class AddExtraDataToTrainingPackage < ActiveRecord::Migration[7.1]
+  def change
+    add_column :training_packages, :extra, :jsonb, default: {}, null: false
+    add_index :training_packages, :extra, using: :gin
+  end
+end
