b422c8f609b2084588ad3cf5337e05fe65f1e71f
Timjini <hatim.jini@gmail.com>
2024-11-18 13:49:34 +0100

    restrect the number of trainingpackages returned

--- app/controllers/api/v2/training_packages_controller.rb
+++ app/controllers/api/v2/training_packages_controller.rb
@@ -7,7 +7,7 @@ module Api
       skip_before_action :authenticate_user!
 
       def index
-        @training_packages = TrainingPackage.where(status: 'active')
+        @training_packages = TrainingPackage.where(status: 'active').limit(3)
         render json: @training_packages, status: :ok
       end
     end
