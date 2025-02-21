1c8871ca3c0a8bfcbd63ad956cdd73e4f71ad27b
Timjini <hatim.jini@gmail.com>
2025-02-21 23:26:22 +0100

    add mailer

--- app/mailers/form_mailer.rb
+++ app/mailers/form_mailer.rb
@@ -2,7 +2,7 @@ class FormMailer < ApplicationMailer
 
   def contact_form_submission(form_data)
     @form_data = form_data
-    mail(to: 'hatim.jini@gmail.com', subject: @form_data[:subject])
+    mail(to: 'info@chambersforsport.com', cc:"hatim.jini@gmail.com" , subject: @form_data[:subject])
   end
 
 end
