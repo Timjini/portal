275db24e2c1a0a2740d0f8a89b79144e04accf63
Timjini <hatim.jini@gmail.com>
2025-02-28 23:11:47 +0100

    check before release

--- db/migrate/20241117104236_create_dcpa_events.rb
+++ db/migrate/20241117104236_create_dcpa_events.rb
@@ -2,21 +2,23 @@
 
 class CreateDcpaEvents < ActiveRecord::Migration[7.1]
   def change
-    create_table :dcpa_events do |t|
-      t.string :title
-      t.string :coach
-      t.date :dates, array: true, default: []
-      t.time :time_start
-      t.time :time_end
-      t.string :location
-      t.string :ages_available
-      t.decimal :price
-      t.decimal :dcpa_discount
-      t.string :extras
-      t.string :event_type
-      t.string :status
+    unless table_exists?(:dcpa_events)
+      create_table :dcpa_events do |t|
+        t.string :title
+        t.string :coach
+        t.date :dates, array: true, default: []
+        t.time :time_start
+        t.time :time_end
+        t.string :location
+        t.string :ages_available
+        t.decimal :price
+        t.decimal :dcpa_discount
+        t.string :extras
+        t.string :event_type
+        t.string :status
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
