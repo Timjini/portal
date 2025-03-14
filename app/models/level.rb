fba6f51fb5f6eef5fa9e192238d7f4bbb176ce2a
Timjini <hatim.jini@gmail.com>
2025-03-14 12:01:41 +0100

    correct levels

--- app/models/level.rb
+++ app/models/level.rb
@@ -7,5 +7,5 @@ class Level < ApplicationRecord
   enum :degree, { development: 0, intermediate: 1, advanced: 2 }
   enum :category, { information_management: 0, technical_ability: 1, physical_strength: 2, coordination: 3,
                     performance_time: 4 }
-  enum :step, { one: 1, two:2, three:3, four:4, five:5}
+  enum :step, { one: 1, two: 2, three: 3, four: 4, five: 5 }
 end
--- app/services/athlete_checklist_status_service.rb
+++ app/services/athlete_checklist_status_service.rb
@@ -6,7 +6,7 @@ class AthleteChecklistStatusService
     @user = User.find_by(id: @athlete.user_id)
   end
 
-  def call
+  def call # rubocop:disable Metrics/MethodLength
     levels = fetch_levels
     levels_count = UserLevel.where(user_id: @user.id).count
     percentage = calculate_percentage(levels_count)
@@ -28,9 +28,9 @@ class AthleteChecklistStatusService
 
   def fetch_levels
     permitted_params = %w[title degree category step]
-    hash = @params.to_unsafe_h 
+    hash = @params.to_unsafe_h
     cleaned_params = hash.slice(*permitted_params).compact
-    Level.where(cleaned_params).order(:degree, :step)
+    Level.where(cleaned_params).order(:degree, :step, :category)
   end
 
   def calculate_percentage(levels_count)
@@ -38,7 +38,7 @@ class AthleteChecklistStatusService
     (levels_count / total) * 100
   end
 
-  def determine_level_status(levels)
+  def determine_level_status(levels) # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     athlete_level = @athlete.athlete_level.where(status: 'completed')
     status = {}
 
