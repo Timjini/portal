c4f828cb694919b07c4fce7a138ad2d34416fe34
Timjini <hatim.jini@gmail.com>
2025-03-18 20:14:34 +0100

    chicago camp

---
+++ app/serializers/api/v2/training_package_serializer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Api
+  module V2
+    class TrainingPackageSerializer < ActiveModel::Serializer
+      attributes :id, :name, :description, :features, :price, :duration_in_days, :package_type, :training_type, 
+                 :duration, :status, :extra, :formatted_start_date, :formatted_end_date
+      def formatted_start_date
+        object.start_date.strftime('%a %d %B')
+      end
+
+      def formatted_end_date
+        object.ending_date.strftime('%a %d %B')
+      end
+    end
+  end
+end
---
+++ db/migrate/20250318184021_add_dates_to_training_packages.rb
@@ -0,0 +1,6 @@
+class AddDatesToTrainingPackages < ActiveRecord::Migration[7.1]
+  def change
+    add_column :training_packages, :start_date, :date # rubocop:disable Rails/BulkChangeTable
+    add_column :training_packages, :ending_date, :date
+  end
+end
