22ece9ae3d2d5b6cf727653d377c5ea6e40978e3
Timjini <hatim.jini@gmail.com>
2025-06-19 02:10:01 +0200

    controller issue

--- app/controllers/assesment_center_controller.rb
+++ app/controllers/assesment_center_controller.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-class Users::AssessmentsCenterController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
+class AssessmentsCenterController < ApplicationController # rubocop:disable Style/ClassAndModuleChildren
   def index; end
 end
