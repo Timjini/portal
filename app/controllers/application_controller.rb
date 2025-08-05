84ac7dd00e49223012aef462d720949760679aff
Timjini <hatim.jini@gmail.com>
2025-08-06 00:27:47 +0200

    detected bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,7 +68,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     if resource.admin?
-      admins_dashboard_path
+      admin_dashboard_index_path
     else
       stored_location_for(resource) || authenticated_root_path
     end
