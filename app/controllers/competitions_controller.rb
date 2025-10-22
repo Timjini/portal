babeac54b2158f02fc342c52767c863f19276bdd
Timjini <hatim.jini@gmail.com>
2025-10-22 21:37:22 +0200

    CP-103

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -73,6 +73,6 @@ class CompetitionsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def competition_params
-    params.require(:competition).permit(:title, :date, :link, :image, :status)
+    params.require(:competition).permit(:title, :date, :description, :link, :image, :status)
   end
 end
