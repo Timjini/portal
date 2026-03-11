cef174558b2c8aa95fa13abe67cbbe3207ae26a8
Timjini <hatim.jini@gmail.com>
2026-03-11 18:42:48 +0100

    fixing cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,11 +2,19 @@
 
 # cors file
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # allow do
+  #   # origins 'http://localhost:3000'
+  #   origins ['https://www.chambersforsport.com', 'https://www.chambersforsport.com/taster-sessions-booking.php',
+  #            'http://localhost:3000', 'https://website.chambersforsport.com/', 'http://localhost:5173', 'https://4f1t.com', 
+  #            'https://www.4f1t.com']
+  #   resource '*', headers: :any, methods: %i[get post put patch delete options], credentials: true
+  # end
+
   allow do
-    # origins 'http://localhost:3000'
-    origins ['https://www.chambersforsport.com', 'https://www.chambersforsport.com/taster-sessions-booking.php',
-             'http://localhost:3000', 'https://website.chambersforsport.com/', 'http://localhost:5173', 'https://4f1t.com', 
-             'https://www.4f1t.com']
-    resource '*', headers: :any, methods: %i[get post put patch delete options], credentials: true
+    origins %r{\Ahttps://(www|website)\.chambersforsport\.com\z}
+    resource '*', 
+      headers: :any, 
+      methods: %i[get post put patch delete options], 
+      credentials: true
   end
 end
