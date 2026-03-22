727773e6870fc2ad0802b5e36205d743d6afda33
Timjini <hatim.jini@gmail.com>
2026-03-22 06:24:57 +0100

    fixing training booking model

--- app/models/training_booking.rb
+++ app/models/training_booking.rb
@@ -2,7 +2,18 @@
 
 class TrainingBooking < ApplicationRecord
   belongs_to :training_package
-  after_create -> { Rails.logger.info('Congratulations, the callback has run!') }
+  after_create -> { admin_notification_email }
 
   # belongs_to :user
+  before_validation(on: :create) do
+    self.training_package = TrainingPackage.first if training_package.nil?
+  end
+
+  private
+
+  def admin_notification_email
+    mailer = FormMailer.new(self)
+    mailer.contact_form_submission
+    Rails.logger.info('Admin Notification Email sent')
+  end
 end
