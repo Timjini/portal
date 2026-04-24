0740eb43ec1c39ce60cd58b10c7004cf86ad079b
Timjini <hatim.jini@gmail.com>
2026-04-24 12:05:05 +0200

    remove compromised library

--- app/helpers/admins/athlete_levels_helper.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Admins
-  module AthleteLevelsHelper
-  end
-end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+# TODO: This musstt GOOOO!
 module UserHelper
   include AthleteProfilesHelper
 
